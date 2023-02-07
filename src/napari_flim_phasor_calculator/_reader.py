927fb5b5037fdb1083db42d47e946a95ed8bfce4
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-02-07 18:18:40 +0100

    add summed intensity layer
    - currently duplicates metadata of ptu files over layers (not sure if channels can have different metadata, like different laser frequencies in ptu files)

--- src/napari_flim_phasor_calculator/_reader.py
+++ src/napari_flim_phasor_calculator/_reader.py
@@ -8,6 +8,7 @@ https://napari.org/stable/plugins/guides.html?#readers
 import numpy as np
 from napari_flim_phasor_calculator._io.readPTU_FLIM import PTUreader
 import sdtfile
+from pathlib import Path
 
 def napari_get_reader(path):
     """A basic implementation of a Reader contribution.
@@ -71,6 +72,8 @@ def flim_file_reader(path):
     paths = [path] if isinstance(path, str) else path
     layer_data = []
     for path in paths:
+        # create list of metadata for each channel
+        metadata_list = []
         # get data from ptu files
         if path.lower().endswith('.ptu'):
             ptu_file = PTUreader(path, print_header_data = False)
@@ -78,26 +81,31 @@ def flim_file_reader(path):
             # Move xy dimensions to the end
             # TO DO: handle 3D images
             data = np.moveaxis(data, [0, 1], [-2, -1])
+            intensity_image = np.sum(data, axis=1) # sum over photon_time axis
             # optional kwargs for the corresponding viewer.add_* method
             # TO DO: get laser frequency for multiple channels, similar to 
-            # how it was done for sdt below
+            # how it was done for sdt below. Currently duplicating metadata.
             metadata = ptu_file.head
             metadata['file_type'] = 'ptu'
-
+            # Add same metadata to each channel
+            for channel in range(data.shape[0]):
+                metadata_list.append(metadata)
         # get data from sdt files
-        else:
+        elif path.lower().endswith('.sdt'):
             sdt_file = sdtfile.SdtFile(path)  # header to be implemented
             data_raw = np.asarray(sdt_file.data)  # option to choose channel to include
-            data = np.moveaxis(np.stack(data_raw), 3, 1)
+            data = np.moveaxis(np.stack(data_raw), -1, 1)
+            intensity_image = np.sum(data, axis=1) # sum over photon_time axis
 
-            # create list of metadata for each channel
-            metadata_list = []
             for measure_info_recarray in sdt_file.measure_info:
                 metadata = {'measure_info': recarray_to_dict(measure_info_recarray),
                             'file_type': 'sdt'}
                 metadata_list.append(metadata)
-
+        # arguments for TCSPC stack
         add_kwargs = {'channel_axis': 0, 'metadata': metadata_list}
         layer_type = "image"
         layer_data.append((data, add_kwargs, layer_type))
+        # arguments for intensity image
+        add_kwargs = {'channel_axis': 0, 'metadata': metadata_list, 'name': 'intensity_image_' + Path(path).stem}
+        layer_data.append((intensity_image, add_kwargs, layer_type))
     return layer_data
