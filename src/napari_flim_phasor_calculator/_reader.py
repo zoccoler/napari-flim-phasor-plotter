877ae639b0a1bc6b3e5ad00442a480e83e579e4d
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-02-28 16:39:33 +0100

    testing dask
    - numpy stack working
    - full dask stack works, but it is slow
    - dask for each timepoint does not work

--- src/napari_flim_phasor_calculator/_reader.py
+++ src/napari_flim_phasor_calculator/_reader.py
@@ -121,16 +121,35 @@ def flim_file_reader(path):
         layer_data.append((summed_intensity_image, add_kwargs, layer_type))
     return layer_data
 
+def get_max_zslices(file_paths):
+    max_z = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.ptu'])[1]
+    if max_z is None:
+        return 1
+    return max_z
+def get_max_time(file_paths):
+    max_time = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.ptu'])[0]
+    if max_time is None:
+        return 1
+    return max_time
+def get_stack_estimated_sizes(file_paths):
+    max_time = get_max_time(file_paths)
+    max_z = get_max_zslices(file_paths)
+    time_stack_size = 0
+    for file_path in file_paths:
+        if file_path.suffix == '.ptu':
+            file_size = file_path.stat().st_size / 1e6 # in MB
+            time_stack_size += file_size
+    z_stack_estimated_size = time_stack_size / max_time
+    
+    return z_stack_estimated_size, time_stack_size
+
 def read_ptu_stack(path):
     # Get list of file paths
     file_paths = natsorted([file_path for file_path in path.iterdir()])
-    # # Sort file_paths
-    # pattern_t = '_t(\d+)' # numbers following '_t'
-    # pattern_z = '_z(\d+)' # numbers following '_z'
-    # file_paths = sorted(file_paths0, key=lambda x: (int(re.findall(pattern_t, x.stem)[0]) if re.findall(pattern_t, x.stem) else 0,
-    #                                         int(re.findall(pattern_z, x.stem)[0]) if re.findall(pattern_z, x.stem) else 0))
-
-    tz_max = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.ptu'])
+    # Get maximum time and z (returns None if not present)
+    max_time, max_z = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.ptu'])
+    # Estimate stack sizes
+    z_stack_estimated_size, time_stack_size = get_stack_estimated_sizes(file_paths)
     
     # TO DO: offer fast reading option by calculating max shape from metadata (array may become bigger)
     # Go through files to get max shape (number of photon bins may vary from image to image)
@@ -142,72 +161,204 @@ def read_ptu_stack(path):
                                 np.unique(ptu_file.tcspc).size,   # number of photon bins
                                 ptu_file.y_size,                  # y shape
                                 ptu_file.x_size))                 # x shape
-    image_shape = max(shapes_list)
+    # Get image slice shape (ch, mt, y, x)
+    image_slice_shape = max(shapes_list)
+    # Get data type from last ptu file read
     image_dtype = ptu_file.get_flim_data_stack()[0].dtype
-    # image_shape = max([(np.unique(PTUreader(file_path, print_header_data = False).channel).size,
-    #                     np.unique(PTUreader(file_path, print_header_data = False).tcspc).size,
-    #                     PTUreader(file_path, print_header_data = False).y_size,
-    #                     PTUreader(file_path, print_header_data = False).x_size)
-    #                     for file_path in file_paths if file_path.suffix == '.ptu'])
 
-    # Make a dask stack
-    z_list, t_list, z_summed_intensity_list, t_summed_intensity_list = [], [], [], []
+    # Choose whether to read full stack or to make dask arrays
+    # If full stack is small, read as numpy (using 1 for now for testing)
+    if time_stack_size < 1:#8e3: #8GB
+        # read full stack
+        data = make_full_numpy_stack(file_paths, image_slice_shape, image_dtype)
+    else:
+        # If z_stack small, make a dask array where each chunk is a timepoint (single chunck for single timepoint)
+        # NOT WORKING, rechunking too slow, test MINIMAL EXAMPLE IN JUPYTER NOTEBOOK
+        if z_stack_estimated_size < 8e3: # 10GB
+            # If z_stack_estimated_size is not big:
+            image_stack_shape = list(image_slice_shape)
+            image_stack_shape.insert(0, max_z)
+            image_stack_shape = tuple(image_stack_shape)
+            data = make_dask_timelapse_of_3D_stacks(file_paths, image_stack_shape, image_dtype)
+        # If zstack is too large, then make a dask array where each chunk is a z-slice (not practicak with napari, becomes too slow)
+        else:
+            data = make_dask_timelapse_of_dask_2D_slices(file_paths, image_slice_shape, image_dtype)
+    # maks summed intensity from second dimension (axis=1) (microtime)
+    summed_intensity_image = da.sum(data, axis=1)
+  
+    # The code below works!! But napari becomes super slow when moving the sliders...
+    # # Make a dask stack
+    # z_list, t_list, z_summed_intensity_list, t_summed_intensity_list = [], [], [], []
+    # previous_t = 1
+    # for file_path in file_paths:
+    #     if file_path.suffix == '.ptu':
+    #         current_t, current_z = get_current_tz(file_path)
+    #         #If time changed, append to t_list and clear z_list
+    #         if current_t is not None:
+    #             if current_t > previous_t:
+    #                 t_list.append(da.stack(z_list, axis=0))
+    #                 # t_summed_intensity_list.append(da.stack(z_summed_intensity_list, axis=0))
+    #                 z_list = []
+    #                 z_summed_intensity_list = []
+    #                 previous_t = current_t
+    #         # TO DO: delayed reading with dask
+    #         # TO DO: reading function should return single image, not tuple
+    #         data = da.from_delayed(read_ptu_data_2D_delayed(file_path), shape=image_shape, dtype=image_dtype)
+    #         summed_intensity_image = da.from_delayed(read_ptu_summed_intensity_image_delayed(file_path), 
+    #                                                  shape=tuple([x for i,x in enumerate(image_shape) if i!=1]),
+    #                                                  dtype=image_dtype)
+    #         # summed_intensity_image
+    #         # metadata_list
+    #         if current_z is not None:
+    #             print(file_path.stem)
+    #             print(data.shape)
+    #             # TO DO: Add as delayed?
+    #             # image = np.zeros(image_shape, dtype=np.uint16)
+    #             # # Try some broadcast function instead
+    #             # image[:data.shape[0], :data.shape[1], :data.shape[2], :data.shape[3]] = data
+    #             z_list.append(data)
+    #             z_summed_intensity_list.append(summed_intensity_image)
+    # # Append last time point
+    # t_list.append(da.stack(z_list, axis=0))
+    # t_summed_intensity_list.append(da.stack(z_summed_intensity_list, axis=0))
+    # data = da.stack(t_list)
+    # summed_intensity_image = da.stack(t_summed_intensity_list)
+    # data =  da.moveaxis(data, [-4, -3], [0, 1])
+    # summed_intensity_image = da.moveaxis(summed_intensity_image, -3, 0)
+
+    metadata_list = []
+    return data, summed_intensity_image, metadata_list
+
+@dask.delayed
+def read_ptu_data_2D_delayed(path):
+    return read_ptu_data_2D(path)
+
+def read_ptu_data_2D(path):
+    ptu_file = PTUreader(path, print_header_data = False)
+    image, _ = ptu_file.get_flim_data_stack()
+    # move x,y to the end
+    image = np.moveaxis(image, [0, 1], [-2, -1])
+    return image
+
+@dask.delayed
+def read_ptu_data_3D_delayed(file_paths, image_slice_shape):
+    return read_ptu_3D_data(file_paths, image_slice_shape)
+
+def read_ptu_3D_data(file_paths, image_slice_shape):
+    z_list = []
+    for file_path in file_paths:
+        if file_path.suffix == '.ptu':
+            current_t, current_z = get_current_tz(file_path)
+            data = read_ptu_data_2D(file_path)
+            if current_z is not None:
+                image = np.zeros(image_slice_shape, dtype=np.uint16)
+                image[:data.shape[0], :data.shape[1], :data.shape[2], :data.shape[3]] = data
+                z_list.append(image)
+    image_3D = np.stack(z_list)
+    # move channel and microtime to the beginning (putting z behind them): (ch, mt, z, y, x)
+    image_3D = np.moveaxis(image_3D, [-4, -3], [0, 1])
+    return image_3D
+
+def make_full_numpy_stack(file_paths, image_slice_shape, image_dtype):
+     # Make a dask stack
+    z_list, t_list = [], []
     previous_t = 1
     for file_path in file_paths:
         if file_path.suffix == '.ptu':
-            current_z, current_t = get_current_tz(file_path)
+            current_t, current_z = get_current_tz(file_path)
             #If time changed, append to t_list and clear z_list
             if current_t is not None:
                 if current_t > previous_t:
-                    t_list.append(da.stack(z_list, axis=0))
-                    # t_summed_intensity_list.append(da.stack(z_summed_intensity_list, axis=0))
+                    z_stack = np.stack(z_list)
+                    t_list.append(z_stack)
                     z_list = []
-                    z_summed_intensity_list = []
                     previous_t = current_t
-            # TO DO: delayed reading with dask
-            # TO DO: reading function should return single image, not tuple
-            data = da.from_delayed(read_ptu_data_delayed(file_path), shape=image_shape, dtype=image_dtype)
-            summed_intensity_image = da.from_delayed(read_ptu_summed_intensity_image_delayed(file_path), 
-                                                     shape=tuple([x for i,x in enumerate(image_shape) if i!=1]),
-                                                     dtype=image_dtype)
+            data = read_ptu_data_2D(file_path)
             # summed_intensity_image
             # metadata_list
             if current_z is not None:
                 print(file_path.stem)
                 print(data.shape)
-                # TO DO: Add as delayed?
-                # image = np.zeros(image_shape, dtype=np.uint16)
-                # # Try some broadcast function instead
-                # image[:data.shape[0], :data.shape[1], :data.shape[2], :data.shape[3]] = data
-                z_list.append(data)
-                z_summed_intensity_list.append(summed_intensity_image)
+                z_slice = np.zeros(image_slice_shape, dtype=image_dtype)
+                z_slice[:data.shape[0], :data.shape[1], :data.shape[2], :data.shape[3]] = data
+                z_list.append(z_slice)
+    # If no timepoints, make zstack
+    if current_t is None:
+        z_stack = np.stack(z_list)
+        t_list.append(z_stack)
+    # Append last time point
+    t_list.append(z_stack)
+    stack = np.stack(t_list)
+    stack =  np.moveaxis(data, [-4, -3], [0, 1])
+    return stack
+    
+def make_dask_timelapse_of_3D_stacks(file_paths, image_stack_shape, image_dtype):
+    # Make a dask stack
+    t_list = []
+    z_path_list = []
+    previous_t = 1
+    for file_path in file_paths:
+        if file_path.suffix == '.ptu':
+            current_t, current_z = get_current_tz(file_path)
+            #If time changed, append to t_list and clear z_list
+            if current_t is not None:
+                if current_t > previous_t:
+                    z_stack = da.from_delayed(read_ptu_data_3D_delayed(z_path_list, image_slice_shape = image_stack_shape[1:]),
+                                              shape=image_stack_shape, dtype=image_dtype)
+                    t_list.append(z_stack)
+                    z_path_list = []
+                    previous_t = current_t
+                else:
+                    z_path_list.append(file_path)
+    # If no timepoints, make zstack
+    if current_t is None:
+        z_path_list = file_paths
+        z_stack = da.from_delayed(read_ptu_data_3D_delayed(z_path_list, image_slice_shape = image_stack_shape[1:]),
+                                              shape=image_stack_shape, dtype=image_dtype)
+    # Append last time point
+    t_list.append(z_stack)
+    dask_timelapse_of_3D_stacks = da.stack(t_list)
+    chunk = (len(t_list), image_stack_shape[0], image_stack_shape[1], 1, 1, 1) # t, z, ch, mt, y, x
+    dask_timelapse_of_3D_stacks = dask_timelapse_of_3D_stacks.rechunk(chunk) # too slow, never finishes....
+    # move channel and microtime to the beginning (putting t behind them): (ch, mt, t, z, y, x)
+    dask_timelapse_of_3D_stacks =  da.moveaxis(dask_timelapse_of_3D_stacks, [-5, -4], [0, 1])
+    return dask_timelapse_of_3D_stacks
+
+def make_dask_timelapse_of_dask_2D_slices(file_paths, image_slice_shape, image_dtype):
+    # Make a dask stack
+    z_list, t_list = [], []
+    previous_t = 1
+    for file_path in file_paths:
+        if file_path.suffix == '.ptu':
+            current_t, current_z = get_current_tz(file_path)
+            #If time changed, append to t_list and clear z_list
+            if current_t is not None:
+                if current_t > previous_t:
+                    t_list.append(da.stack(z_list, axis=0))
+                    z_list = []
+                    previous_t = current_t
+            z_slice = da.from_delayed(read_ptu_data_2D_delayed(file_path), shape=image_slice_shape, dtype=image_dtype)
+            if current_z is not None:
+                print(file_path.stem)
+                print(z_slice.shape)
+                z_list.append(z_slice)
     # Append last time point
     t_list.append(da.stack(z_list, axis=0))
-    t_summed_intensity_list.append(da.stack(z_summed_intensity_list, axis=0))
-    data = da.stack(t_list)
-    summed_intensity_image = da.stack(t_summed_intensity_list)
-    data =  da.moveaxis(data, [-4, -3], [0, 1])
-    summed_intensity_image = da.moveaxis(summed_intensity_image, -3, 0)
-    # summed_intensity_image = np.zeros((10,10))
-    metadata_list = []
-    return data, summed_intensity_image, metadata_list
+    dask_timelapse_of_dask_2D_slices = da.stack(t_list)
+    dask_timelapse_of_dask_2D_slices =  da.moveaxis(dask_timelapse_of_dask_2D_slices, [-4, -3], [0, 1])
+    return dask_timelapse_of_dask_2D_slices
 
-@dask.delayed
-def read_ptu_data_delayed(path):
-    return read_ptu_data(path)
 
-def read_ptu_data(path):
-    ptu_file = PTUreader(path, print_header_data = False)
-    data, _ = ptu_file.get_flim_data_stack()
-    data = np.moveaxis(data, [0, 1], [-2, -1])
-    return data
 
+
+
+# To HANDLE LATER
 @dask.delayed
 def read_ptu_summed_intensity_image_delayed(path):
     return read_ptu_summed_intensity_image(path)
 
 def read_ptu_summed_intensity_image(path):
-    data = read_ptu_data(path)
+    data = read_ptu_data_2D(path)
     summed_intensity_image = np.sum(data, axis=1) # sum over photon_time axis
     return summed_intensity_image
 
@@ -217,8 +368,7 @@ def read_ptu_metadata(path):
     metadata['file_type'] = 'ptu'
     return metadata
 
-
-
+# Read single ptu file
 def read_ptu_file(path):
     # create list of metadata for each channel
     metadata_list = []
@@ -249,4 +399,4 @@ def get_current_tz(file_path):
     matches_t = re.search(pattern_t, file_name)
     if matches_t is not None:
         current_t = int(matches_t.group(1))
-    return current_z, current_t
+    return current_t, current_z
