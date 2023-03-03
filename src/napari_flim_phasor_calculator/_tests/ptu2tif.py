12b7bc693e5da28283d5bf133f6719cfe89d07a1
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-03-03 18:48:22 +0100

    add tests to read tif stacks
    - ptu2tif sequentially converts all pet to tif in a folder
    - test_dask_zstack_lazy_loading reads 3D timelapse of tif (lazy part with dask to be tested yet)

---
+++ src/napari_flim_phasor_calculator/_tests/ptu2tif.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import numpy as np
+from skimage import io
+from napari_flim_phasor_calculator._reader import read_ptu_data_2D
+
+folder_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack"
+# Use Path from pathlib
+folder_path = Path(folder_path)
+output_path = folder_path.parent / 'output_as_tif'
+output_path.mkdir(exist_ok = True)
+
+for path in folder_path.iterdir():
+    if path.suffix == '.ptu':
+        print(path.stem)
+        image = read_ptu_data_2D(path)
+        file_name = path.stem + '.tif'
+        io.imsave(output_path / file_name, image)
+
+        
---
+++ src/napari_flim_phasor_calculator/_tests/test_dask_zstack_lazy_loading.py
@@ -0,0 +1,139 @@
+from pathlib import Path
+import napari
+import dask.array as da
+import dask
+from natsort import natsorted
+import numpy as np
+from napari_flim_phasor_calculator._reader import get_current_tz
+
+folder_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\output_as_tif"
+folder_path = Path(folder_path)
+
+def read_tif_data_2D(path):
+    from skimage.io import imread
+    image = imread(path)
+    return image
+
+def read_tif_3D_data(file_paths, image_slice_max_shape):
+    file_paths = natsorted(file_paths)
+    z_list = []
+    for file_path in file_paths:
+        if file_path.suffix == '.tif':
+            current_t, current_z = get_current_tz(file_path)
+            data = read_tif_data_2D(file_path)
+            if current_z is not None:
+                image = np.zeros(image_slice_max_shape, dtype=data.dtype)
+                image[:data.shape[0], :data.shape[1], :data.shape[2], :data.shape[3]] = data
+                z_list.append(image)
+    image_3D = np.stack(z_list)
+    # move channel and microtime to the beginning (putting z behind them): from (z, ch, mt, y, x) to (ch, mt, z, y, x)
+    image_3D = np.moveaxis(image_3D, [-4, -3], [0, 1])
+    return image_3D
+
+@dask.delayed
+def read_tif_data_2D_delayed(path):
+    return read_tif_data_2D(path)
+
+@dask.delayed
+def read_tif_data_3D_delayed(file_paths, image_slice_shape):
+    return read_tif_3D_data(file_paths, image_slice_shape)
+
+# Read all slices to get slice max shape and dtype
+slice_shape_list = []
+for file_path in folder_path.iterdir():
+    if file_path.suffix == '.tif':
+        image_2D = read_tif_data_2D(file_path)
+        slice_shape_list.append(image_2D.shape)
+slice_max_shape = max(slice_shape_list)
+print('last_slice_shape = ', image_2D.shape,'image_dtype = ', image_2D.dtype)
+print('max_slice_shape = ', slice_max_shape)
+
+# Get max z slices by reading all file names
+def get_max_zslices(file_paths):
+    max_z = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.tif'])[1]
+    if max_z is None:
+        return 1
+    return max_z
+def get_max_time(file_paths):
+    max_time = max([get_current_tz(file_path) for file_path in file_paths if file_path.suffix == '.tif'])[0]
+    if max_time is None:
+        return 1
+    return max_time
+file_paths = [file_path for file_path in folder_path.iterdir() if file_path.suffix == '.tif']
+max_z = get_max_zslices(file_paths)
+print('max_z slices = ', max_z)
+max_t = get_max_time(file_paths)
+print('max_time = ', max_t)
+
+# Read 3D data without dask
+file_paths = [file_path for file_path in folder_path.iterdir() if file_path.suffix == '.tif']
+# image_3D = read_tif_3D_data(file_paths, image_slice_max_shape=slice_max_shape)
+# print('image_3D shape = ', image_3D.shape)
+
+# Make timelapse (single timepoint in this case) of 3D stacks
+def make_timelapse_of_3D_stacks(file_paths, image_stack_shape):
+    # Make a dask stack
+    t_list = []
+    z_path_list = []
+    previous_t = 1
+    for file_path in file_paths:
+        if file_path.suffix == '.tif':
+            current_t, current_z = get_current_tz(file_path)
+            #If time changed, append to t_list and clear z_list
+            if current_t is not None:
+                if current_t > previous_t:
+                    z_stack = read_tif_3D_data(z_path_list, image_slice_max_shape = image_stack_shape[1:])
+                    t_list.append(z_stack)
+                    z_path_list = []
+                    previous_t = current_t
+                else:
+                    z_path_list.append(file_path)
+    # If no timepoints, make zstack
+    if current_t is None:
+        z_path_list = file_paths
+        z_stack = read_tif_3D_data(z_path_list, image_slice_max_shape = image_stack_shape[1:])
+    # Append last time point
+    t_list.append(z_stack)
+    timelapse_of_3D_stacks = np.stack(t_list)
+    # move channel and microtime to the beginning (putting t behind them): (ch, mt, t, z, y, x)
+    timelapse_of_3D_stacks =  np.moveaxis(timelapse_of_3D_stacks, [-5, -4], [0, 1])
+    return timelapse_of_3D_stacks
+
+
+# Read 3D timelapse data without dask (just a single timepoint for now)
+file_paths = [file_path for file_path in folder_path.iterdir() if file_path.suffix == '.tif']
+image_stack_shape = (max_z, *slice_max_shape)
+image_3D_timelapse = make_timelapse_of_3D_stacks(file_paths, image_stack_shape)
+print('image_3D timelapse shape = ', image_3D_timelapse.shape)
+
+def make_dask_timelapse_of_3D_stacks(file_paths, image_stack_shape, image_dtype):
+    # Make a dask stack
+    t_list = []
+    z_path_list = []
+    previous_t = 1
+    for file_path in file_paths:
+        if file_path.suffix == '.ptu':
+            current_t, current_z = get_current_tz(file_path)
+            #If time changed, append to t_list and clear z_list
+            if current_t is not None:
+                if current_t > previous_t:
+                    z_stack = da.from_delayed(read_ptu_data_3D_delayed(z_path_list, image_slice_shape = image_stack_shape[1:]),
+                                              shape=image_stack_shape, dtype=image_dtype)
+                    t_list.append(z_stack)
+                    z_path_list = []
+                    previous_t = current_t
+                else:
+                    z_path_list.append(file_path)
+    # If no timepoints, make zstack
+    if current_t is None:
+        z_path_list = file_paths
+        z_stack = da.from_delayed(read_ptu_data_3D_delayed(z_path_list, image_slice_shape = image_stack_shape[1:]),
+                                              shape=image_stack_shape, dtype=image_dtype)
+    # Append last time point
+    t_list.append(z_stack)
+    dask_timelapse_of_3D_stacks = da.stack(t_list)
+    chunk = (len(t_list), image_stack_shape[0], image_stack_shape[1], 1, 1, 1) # t, z, ch, mt, y, x
+    dask_timelapse_of_3D_stacks = dask_timelapse_of_3D_stacks.rechunk(chunk) # too slow, never finishes....
+    # move channel and microtime to the beginning (putting t behind them): (ch, mt, t, z, y, x)
+    dask_timelapse_of_3D_stacks =  da.moveaxis(dask_timelapse_of_3D_stacks, [-5, -4], [0, 1])
+    return dask_timelapse_of_3D_stacks
