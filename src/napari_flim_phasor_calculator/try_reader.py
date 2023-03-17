daddadf83a24bc979d1030afafe1eb6026ef995a
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-03-17 17:07:14 +0100

    debugging file

--- src/napari_flim_phasor_calculator/try_reader.py
+++ src/napari_flim_phasor_calculator/try_reader.py
@@ -3,8 +3,9 @@ from pathlib import Path
 viewer = napari.Viewer()
 
 # files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_smaller_as_tif"  # TIF FILES
-files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_smaller"  # PTU FILES
-files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_as_tif\stack_as_tif.zarr"   # ZARR
-# Error plugin cannot read zarr or tif ("no plugin named...")
-viewer.open(files_path, plugin='napari-flim-phasor-calculator')
+# files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_smaller"  # PTU FILES
+# files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_smaller_as_tif\stack_smaller_as_tif.zarr"  # ZARR
+# files_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_as_tif\stack_as_tif.zarr"   # BIG ZARR
+
+# viewer.open(files_path, plugin='napari-flim-phasor-calculator')
 napari.run()
