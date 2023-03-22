b31c640b250e022eb66d4160eeeae7f5f3b033e6
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-03-22 13:21:11 +0100

    remove local tests

--- src/napari_flim_phasor_calculator/_tests/test_reader.py
+++ src/napari_flim_phasor_calculator/_tests/test_reader.py
@@ -2,35 +2,6 @@ import numpy as np
 
 from napari_flim_phasor_calculator import napari_get_reader
 
-# To do: Get a sample file to test reader
-
-import napari
-from pathlib import Path
-viewer = napari.Viewer()
-ptu_image_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\copied_for_Marcelo\single_image\raw_FLIM_data\single_FLIM_image.ptu"
-sdt_image_path = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\Sample_files\sperm_seminal_receptacle_NADH_FLIM_2ch.sdt"
-ptu_folder_path_timelapse = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\embryo_FLIM_data\raw_data_embryo_stack_3tps_43pl_2ch\embryo_43pl_2ch_3tps"
-ptu_folder_path_zstack = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack" # neverending reading...
-ptu_folder_path_zstack_smaller = r"C:\Users\mazo260d\Desktop\Conni_BiA_PoL\stack_smaller"
-
-# ptu_image_path = r"Z:\Data\Conni_BiA_PoL\copied_for_Marcelo\single_image\raw_FLIM_data\single_FLIM_image.ptu"
-# sdt_image_path = r"Z:\Data\Conni_BiA_PoL\Sample_files\sperm_seminal_receptacle_NADH_FLIM_2ch.sdt"
-# ptu_folder_path_timelapse = r"Z:\Data\Conni_BiA_PoL\embryo_FLIM_data\raw_data_embryo_stack_3tps_43pl_2ch\embryo_43pl_2ch_3tps"
-# ptu_folder_path_zstack = r"Z:\Data\Conni_BiA_PoL\stack"
-
-# ptu_list = list(ptu_folder_path.glob('**/*.ptu'))
-# viewer.open(ptu_list, stack=True)
-
-# Open single sdt image
-# viewer.open(sdt_image_path, plugin='napari-flim-phasor-calculator')
-# # Open single ptu image
-# viewer.open(ptu_image_path, plugin='napari-flim-phasor-calculator')
-# Open z-stack
-# viewer.open(ptu_folder_path_zstack, plugin='napari-flim-phasor-calculator')
-# open time-lapse z-stack
-viewer.open(ptu_folder_path_zstack_smaller, plugin='napari-flim-phasor-calculator')
-napari.run()
-
 
 # # tmp_path is a pytest fixture
 # def test_reader(tmp_path):
