f591797cb81e688388953b4c7dc4b238253f42e6
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-08-14 15:58:02 +0200

    add seminal receptacle sample data

--- src/napari_flim_phasor_plotter/_sample_data.py
+++ src/napari_flim_phasor_plotter/_sample_data.py
@@ -19,3 +19,29 @@ def make_sample_data():
     # add_image_kwargs
     # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
     return [(numpy.random.rand(512, 512), {})]
+
+
+def load_seminal_receptacle_image():
+    """
+    Load a seminal receptacle FLIM single image from a .sdt file.
+
+    This image was published in:
+    Wetzker, Cornelia. (2019).
+    Example sdt raw FLIM images of NAD(P)H autofluorescence in Drosophila melanogaster tissues [Data set].
+    https://doi.org/10.1038/s41598-019-56067-w
+    """
+    from pathlib import Path
+    import numpy as np
+    from napari_flim_phasor_plotter._reader import read_single_sdt_file
+
+    file_path = Path(r"./src/napari_flim_phasor_plotter/data/seminal_receptacle_single_image.sdt")
+    image, metadata = read_single_sdt_file(file_path)
+    image = np.expand_dims(image, axis=(2, 3))  # (ch, ut, t, z, y, x)
+    return [(image, {'name': 'seminal receptacle raw FLIM image',
+                     'metadata': metadata,
+                     'channel_axis': 0,
+                     }),
+            (np.amax(image, axis=1), {'name': 'seminal receptacle intensity image',
+                                      'metadata': metadata,
+                                      }),
+            ]
