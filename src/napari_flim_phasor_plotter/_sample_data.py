dbfd680937e8e72b158e5dc21df6345fd911ce4f
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-08-15 08:47:50 +0200

    add hazelnut sample data

--- src/napari_flim_phasor_plotter/_sample_data.py
+++ src/napari_flim_phasor_plotter/_sample_data.py
@@ -12,6 +12,15 @@ def load_seminal_receptacle_image():
     Wetzker, Cornelia. (2019).
     Example sdt raw FLIM images of NAD(P)H autofluorescence in Drosophila melanogaster tissues [Data set].
     https://doi.org/10.1038/s41598-019-56067-w
+
+    Returns
+    -------
+    list_of_tuples_of_images_and_metadata : List[LayerDataTuple]
+        A list of tuples, each tuple containing an image and its metadata.
+        The first tuple contains the raw FLIM image with dimensions (ch, ut, t, z, y, x) and metadata.
+        The second tuple contains the intensity image with dimensions (ch, t, z, y, x) and metadata.
+        Channel, time and z dimensions are all of length 1.
+        If loaded into napari, channel dimension is used to split image into layers.
     """
     import numpy as np
     from napari_flim_phasor_plotter._reader import read_single_sdt_file
@@ -22,8 +31,42 @@ def load_seminal_receptacle_image():
     return [(image, {'name': 'seminal receptacle raw FLIM image',
                      'metadata': metadata,
                      'channel_axis': 0,
+                     'contrast_limits': (np.amin(image[:, image.shape[1] // 2, ...]),
+                                         np.amax(image[:, image.shape[1] // 2, ...])),
                      }),
             (np.amax(image, axis=1), {'name': 'seminal receptacle intensity image',
+                                      'metadata': metadata,
+                                      }),
+            ]
+
+
+def load_hazelnut_image():
+    """
+    Load a hazelnut FLIM single image from a .ptu file.
+
+    Returns
+    -------
+    list_of_tuples_of_images_and_metadata : List[LayerDataTuple]
+        A list of tuples, each tuple containing an image and its metadata.
+        The first tuple contains the raw FLIM image with dimensions (ch, ut, t, z, y, x) and metadata.
+        The second tuple contains the intensity image with dimensions (ch, t, z, y, x) and metadata.
+        Channel, time and z dimensions are all of length 1.
+        If loaded into napari, channel dimension is used to split image into layers.
+    """
+    import numpy as np
+    from napari_flim_phasor_plotter._reader import read_single_ptu_file
+
+    file_path = DATA_ROOT / "hazelnut_FLIM_single_image.ptu"
+    image, metadata = read_single_ptu_file(file_path)
+    image, metadata = image[0], metadata[0]  # Use first channel, second detector is empty
+    image = np.expand_dims(image, axis=(0, 2, 3))  # (ch, ut, t, z, y, x)
+    return [(image, {'name': 'hazelnut raw FLIM image',
+                     'metadata': metadata,
+                     'channel_axis': 0,
+                     'contrast_limits': (np.amin(image[:, image.shape[1] // 2, ...]),
+                                         np.amax(image[:, image.shape[1] // 2, ...])),
+                     }),
+            (np.amax(image, axis=1), {'name': 'hazelnut intensity image',
                                       'metadata': metadata,
                                       }),
             ]
