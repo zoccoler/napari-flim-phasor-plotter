d3cf5bce9e8e2f349906b91eb88e9cb44fe2e008
Marcelo Zoccoler <marcelozoc@gmail.com>
2023-08-15 08:18:55 +0200

    remove previous random sample data and update path to file

--- src/napari_flim_phasor_plotter/_sample_data.py
+++ src/napari_flim_phasor_plotter/_sample_data.py
@@ -1,24 +1,7 @@
-"""
-This module is an example of a barebones sample data provider for napari.
-
-It implements the "sample data" specification.
-see: https://napari.org/stable/plugins/guides.html?#sample-data
-
-Replace code below according to your needs.
-"""
 from __future__ import annotations
+from pathlib import Path
 
-import numpy
-
-
-def make_sample_data():
-    """Generates an image"""
-    # Return list of tuples
-    # [(data1, add_image_kwargs1), (data2, add_image_kwargs2)]
-    # Check the documentation for more information about the
-    # add_image_kwargs
-    # https://napari.org/stable/api/napari.Viewer.html#napari.Viewer.add_image
-    return [(numpy.random.rand(512, 512), {})]
+DATA_ROOT = Path(__file__).parent / "data"
 
 
 def load_seminal_receptacle_image():
@@ -30,11 +13,10 @@ def load_seminal_receptacle_image():
     Example sdt raw FLIM images of NAD(P)H autofluorescence in Drosophila melanogaster tissues [Data set].
     https://doi.org/10.1038/s41598-019-56067-w
     """
-    from pathlib import Path
     import numpy as np
     from napari_flim_phasor_plotter._reader import read_single_sdt_file
 
-    file_path = Path(r"./src/napari_flim_phasor_plotter/data/seminal_receptacle_FLIM_single_image.sdt")
+    file_path = DATA_ROOT / "seminal_receptacle_FLIM_single_image.sdt"
     image, metadata = read_single_sdt_file(file_path)
     image = np.expand_dims(image, axis=(2, 3))  # (ch, ut, t, z, y, x)
     return [(image, {'name': 'seminal receptacle raw FLIM image',
