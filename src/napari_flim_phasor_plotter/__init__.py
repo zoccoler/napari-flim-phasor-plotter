249d7f599b5dd92a75c7028c463dec8148f10f59
Marcelo Zoccoler <marcelozoc@gmail.com>
2024-08-12 11:13:13 +0200

    bump version

--- src/napari_flim_phasor_plotter/__init__.py
+++ src/napari_flim_phasor_plotter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from ._reader import napari_get_reader
 from ._sample_data import load_seminal_receptacle_image, load_hazelnut_image, load_hazelnut_z_stack, load_lifetime_cat_synthtetic_single_image
